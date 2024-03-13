198733d82ceb764af34d7c124791587875dbc0be
harifa123 <harifarifu@gmail.com>
2024-03-13 12:43:46 +0530

    member add and view

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const adminRouter = require("./controllers/adminRouter")
+const memberRouter = require("./controllers/memberRouter")
 
 const app = express()
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.json())
 
 
 app.use("/api/admin", adminRouter)
+app.use("/api/member", memberRouter)
 
 app.listen(port, () => {
     console.log("Server Running")
